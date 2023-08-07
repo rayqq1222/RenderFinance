5b355a6f839327bb94d2d620ce23288e8c15b5df
rayqq1222 <138569655+rayqq1222@users.noreply.github.com>
2023-08-08 02:23:58 +0800

    Add files via upload

--- events/oil2.py
+++ events/oil2.py
@@ -2,12 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def oil_price():
-    target_url = 'https://icard.ai/blog/featured-post/gasprice/'
+    target_url = 'https://www.cpc.com.tw/'
     rs = requests.session()
     res = rs.get(target_url, verify=False)
     res.encoding = 'utf-8'
     soup = BeautifulSoup(res.text, 'html.parser')
-    title = soup.select('h2.elementor-heading-title elementor-size-default b').text
+    title1 = soup.find('h2.elementor-heading-title elementor-size-default b').text
+    title2 = soup.select
+    title3 = soup.select
     #gas_price = soup.select('#gas-price')[0].text.replace('\n\n\n', '').replace(' ', '')
     #cpc = soup.select('#cpc')[0].text.replace(' ', '')
     content = '{}\n{}{}'.format(title)
---
+++ events/oil3.py
@@ -0,0 +1,22 @@
+def oil_price():
+    url = 'https://www2.moeaboe.gov.tw/oil111/'
+    from bs4 import BeautifulSoup
+    import requests
+
+    re = requests.get(url)
+    soup = BeautifulSoup(re.text, 'lxml')
+    #'html.parser'
+
+    list1 = soup.find_all('ul', class_="cont_18")[6]
+    #print(list1.prettify())
+
+    items = list1.find_all('li')
+    #print(len(items))
+
+
+    for item in items:
+        title = item.find('div', class_='col-4')
+        price = item.find('strong')
+        unit = item.find('small')
+        content = ('%s%s%s' %(title.text.replace(" ",'').replace('\n\n\n', ''), price.text.replace(" ",'').replace('\n\n', ''), unit.text))
+        return content
