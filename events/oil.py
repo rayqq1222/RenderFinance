160c99334f92b263ea841c3b01f825dab8c7e0e8
rayqq1222 <138569655+rayqq1222@users.noreply.github.com>
2023-08-07 16:20:16 +0800

    Add files via upload

--- events/oil.py
+++ events/oil.py
@@ -6,7 +6,7 @@ def oil_price():
     rs = requests.session()
     res = rs.get(target_url, verify=False)
     res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parse')
+    soup = BeautifulSoup(res.text, 'html.parser')
     title = soup.select('#main')[0].text.replace('\n', '').split('(')[0]
     gas_price = soup.select('#gas-price')[0].text.replace('\n\n\n', '').replace(' ', '')
     cpc = soup.select('#cpc')[0].text.replace(' ', '')
