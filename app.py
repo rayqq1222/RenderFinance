e9636e022e79680315ce523dfae71560bbdeedac
rayqq1222 <138569655+rayqq1222@users.noreply.github.com>
2023-08-07 12:54:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -29,21 +29,25 @@ def callback():
 
     return 'OK'
 
-#處理訊息
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    
+    # 自動回覆相同訊息
+    # message = TextSendMessage(text=event.message.text)
+    # line_bot_api.reply_message(event.reply_token, message)
+
+    # 自己設定回覆訊息
     emoji = [
-        {
-            "index" : 0,
-            "productId" : "5ac21184040ab15980c9b43a",
-            "emojiID" : "255"
-        },
-        {
-            "index" : 17,
-            "productId" : "5ac21184040ab15980c9b43a",
-            "emojiID" : "225"
-        }
+            {
+                "index": 0,
+                "productId": "5ac2280f031a6752fb806d65",
+                "emojiID": "005"
+            },
+            {
+                "index": 17,
+                "productId": "5ac2280f031a6752fb806d65",
+                "emojiID": "005"
+            }
     ]
 
     text_message = TextSendMessage(text='''$ Finance Widget $
@@ -53,17 +57,16 @@ Hello! 歡迎您成為 Finance Widget 的好友！
                                        
 -這裡有股票、油價和匯率資訊喔~
 -請直接點選下方【Finance Widget】的選單功能
--期待您的使用！ ''', emojis=emoji)
+-期待您的使用！ ''', emojis = emoji)
     
     sticker_message = StickerSendMessage(
-        package_id='8522',
-        sticker_id='16581271'
+        package_id = '11539',
+        sticker_id ='52114118'
     )
     line_bot_api.reply_message(
         event.reply_token,
         [text_message, sticker_message])
+                                   
 
-                                       
 if __name__ == "__main__":
     app.run()
-
